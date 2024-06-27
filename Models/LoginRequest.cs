f165cf21ad558bebf84879db083d67979e0c87b2
Uchechukwu Offiah <evaristusoffiah@gmail.com>
2024-06-27 20:40:02 +0100

    Added loging request model

---
+++ Models/LoginRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace GeodicBankAPI.Models
+{
+    public class LoginRequest
+    {
+            public string? Username { get; set; }
+            public string? Password { get; set; }
+            public string? AuthKey { get; set; }
+
+    }
+}
