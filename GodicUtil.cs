70e7fb32c0ee22f8f71a220635ff8f6f9266d86e
Uchechukwu Offiah <evaristusoffiah@gmail.com>
2024-06-29 23:01:47 +0100

    Added GodicUtil file

---
+++ GodicUtil.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GeodicBankAPI
+{
+    public static class GodicUtil
+    {
+        public static void LogStuff(string stuff, string folderName)
+        {
+            var directory = $"C:/{folderName}/";
+            var fileName = DateTime.Now.Year.ToString() + "_" + DateTime.Now.Month.ToString() + "_" + DateTime.Now.DayOfYear.ToString() + "_" + "Log.txt";
+            if (!System.IO.Directory.Exists(directory))
+            {
+                System.IO.Directory.CreateDirectory(directory);
+            }
+
+            System.IO.File.AppendAllText(directory + fileName, Environment.NewLine + DateTime.Now.ToString() + Environment.NewLine + stuff + Environment.NewLine + Environment.NewLine);
+        }
+
+        public static async Task SendSms(string receiverNumber, string body, string apiToken, string from, string patientId)
+        {
+            //Util.LogStuff("url = " + url);
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.Timeout = System.Threading.Timeout.InfiniteTimeSpan;
+                    using (HttpResponseMessage res = await client.PostAsync(@"https://www.bulksmsnigeria.com/api/v1/sms/create", new JsonContent(new { api_token = apiToken, from, body, to = receiverNumber, dnd = 2 })).ConfigureAwait(false))
+                    using (HttpContent content = res.Content)
+                    {
+                        var response = await content.ReadAsStringAsync().ConfigureAwait(false);
+                        LogStuff(response, "GodicApiLogs");
+                        res.EnsureSuccessStatusCode();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogStuff(ex.ToString(), "GodicApiLogs");
+            }
+
+        }
+    }
+}
