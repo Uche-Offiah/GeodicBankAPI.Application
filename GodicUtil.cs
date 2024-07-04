15047c0ec226223da339507e6cca0bd8d0156839
Uchechukwu Offiah <evaristusoffiah@gmail.com>
2024-07-04 21:20:34 +0100

    error fix on GodicUtil

--- GodicUtil.cs
+++ GodicUtil.cs
@@ -9,6 +9,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using JsonContent = GeodicBankAPI.Models.JsonContent;
 
 namespace GeodicBankAPI
 {
